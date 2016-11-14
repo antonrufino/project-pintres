811bcbfd2bcbcb3dfdd15495c27d9eb0b6ea3e11
Anton Rufino <anton.vongola@gmail.com>
2016-11-14 21:34:58 +0800

    Create custom directives.

--- frontend/app.js
+++ frontend/app.js
@@ -1,3 +1,16 @@
 (() => {
-    angular.module('app', []);
+    angular.module('app', [])
+        .directive('pinSidebar', () => {
+            return {
+                restrict: 'E',
+                templateUrl: 'views/sidebar.html'
+            };
+        })
+        .directive('pinPostsContainer', () => {
+            return {
+                restrict: 'E',
+                templateUrl: 'views/posts-container.html',
+                controller: 'PostCtrl'
+            }
+        });
 })();
