709f41c1ab3935c6c9e033d90500d3126af362ea
Anton Rufino <anton.vongola@gmail.com>
2016-11-15 01:08:18 +0800

    Add post backend.

--- backend/controllers/post-controller.js
+++ backend/controllers/post-controller.js
@@ -19,6 +19,7 @@ exports.getPost = (req, res) => {
     let query = 'SELECT * FROM posts WHERE id = ?;'
     connection.query(query, [req.params.id], (err, rows) => {
         if (err) {
+            res.status(400).send(rows);
             console.log(err);
         } else {
             res.send(rows);
@@ -30,6 +31,7 @@ exports.getAllPosts = (req, res) => {
     let query = 'SELECT * FROM posts';
     connection.query(query, [], (err, rows) => {
         if (err) {
+            res.status(400).send(rows);
             console.log(err);
         } else {
             res.send(rows);
@@ -38,15 +40,17 @@ exports.getAllPosts = (req, res) => {
 }
 
 exports.addPost = (req, res) => {
-    let query = 'INSERT INTO posts(author_username, author_display_name, post_time, content, topic) VALUES(?, ?, NOW(), ?, ?);'
+    let query = 'INSERT INTO posts(author_username, author_display_name, post_time, content, topic) VALUES(?, ?, ?, ?, ?);'
 
     connection.query(query, [
         req.body.author_username,
         req.body.author_display_name,
+        req.body.post_time,
         req.body.content,
         req.body.topic
     ], (err, rows) => {
         if (err) {
+            res.status(400).send(rows);
             console.log(err);
         } else {
             res.send(rows);
