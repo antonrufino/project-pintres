a02b5723ec5d54aedebf9c77b7b4b71f7c732b5a
Anton Rufino <anton.vongola@gmail.com>
2016-11-16 01:11:12 +0800

    Add users table.

--- backend/pintres.sql
+++ backend/pintres.sql
@@ -11,3 +11,19 @@ CREATE TABLE posts(
     topic VARCHAR(20) NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE users(
+    username VARCHAR(50) NOT NULL,
+    display_name VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    PRIMARY KEY (username)
+);
+
+INSERT INTO users(username, display_name, password)
+VALUES('antonrufino', 'Anton Rufino', PASSWORD('whatpassword'));
+
+INSERT INTO users(username, display_name, password)
+VALUES('czesyeban', 'Czes Yeban', PASSWORD('frontendisheart'));
+
+INSERT INTO users(username, display_name, password)
+VALUES('mariqueentenedero', 'Mariqueen Tenedero', PASSWORD('loginisheart'));
