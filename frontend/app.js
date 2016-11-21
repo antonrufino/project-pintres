b50751bb258f9231766cbf8d14ee60fa9864c4dc
Anton Rufino <anton.vongola@gmail.com>
2016-11-21 11:45:48 +0800

    Merge in czes profile page and add client-side routing.

--- frontend/app.js
+++ frontend/app.js
@@ -1,38 +1,18 @@
 (() => {
-    angular.module('app', [])
-    .directive('pinSidebar', () => {
-        return {
-            restrict: 'E',
-            templateUrl: 'views/sidebar.html',
-            controller: 'SidebarCtrl'
-        };
-    })
-    .directive('pinPostsContainer', () => {
-        return {
-            restrict: 'E',
-            templateUrl: 'views/posts-container.html',
-            controller: 'PostCtrl'
-        }
-    })
-    .directive('pinNewPost', () => {
-        return {
-            restrict: 'E',
-            templateUrl: 'views/new-post.html',
-            controller: 'PostCtrl'
-        }
-    })
-    .directive('pinDeletePost', () => {
-        return {
-            restrict: 'E',
-            templateUrl: 'views/delete-post.html',
-            controller: 'PostCtrl'
-        };
-    })
-    .directive('pinEditPost', () => {
-        return {
-            restrict: 'E',
-            templateUrl: 'views/edit-post.html',
-            controller: 'PostCtrl'
-        };
-    });
+    angular.module('app', ['ngRoute'])
+    .config(['$routeProvider', routes])
+
+    function routes($routeProvider) {
+        $routeProvider
+        .when('/', {
+            redirectTo: '/feed'
+        })
+        .when('/feed', {
+            templateUrl: 'views/feed.html'
+        })
+        .when('/profile', {
+            templateUrl: 'views/profile.html'
+        });
+    }
+
 })();
