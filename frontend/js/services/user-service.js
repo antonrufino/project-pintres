fe6d2fe08ab7c40988c6446057d6f92fd2aac674
Anton Rufino <anton.vongola@gmail.com>
2016-12-05 09:23:59 +0800

    Fix typo.

--- frontend/js/services/user-service.js
+++ frontend/js/services/user-service.js
@@ -10,7 +10,7 @@
             .then((res) => {
                 deferred.resolve(res);
             }, (err) => {
-                deffered.reject(err);
+                deferred.reject(err);
             });
 
             return deferred.promise;
